3efac1b6c8d19bcbe1d534b9fc436ac6a3706319
Ben Lee <benasking7124.en07@nycu.edu.tw>
2022-09-03 15:39:59 +0800

    the clock of tim8 is 168Mhz not 84 Mhz, so I fix the Prescaler from 84 to 168

--- Core/Src/tim.c
+++ Core/Src/tim.c
@@ -177,7 +177,7 @@ void MX_TIM8_Init(void)
 
   /* USER CODE END TIM8_Init 1 */
   htim8.Instance = TIM8;
-  htim8.Init.Prescaler = 84-1;
+  htim8.Init.Prescaler = 168-1;
   htim8.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim8.Init.Period = 3000-1;
   htim8.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
